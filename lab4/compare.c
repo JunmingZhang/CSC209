fac7a70edeff0cd5241169fc9224894a9d911066
Markus <markus@markus.com>
2019-01-28 21:37:29 -0500

    Update starter code files for assignment lab4

---
+++ lab4/compare.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+  Write the main() function of a program that takes exactly two arguments,
+  and prints one of the following:
+    - "Same\n" if the arguments are the same.
+    - "Different\n" if the arguments are different.
+    - "Invalid\n" if the program is called with an incorrect number of
+      arguments.
+
+  Your main function should return 0, regardless of what is printed.
+*/
