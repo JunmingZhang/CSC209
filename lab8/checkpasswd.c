a2ab2698caf4d1d76d0d90f172d92b75c9eaf586
Markus <markus@markus.com>
2019-03-04 22:10:30 -0500

    Update starter code files for assignment lab8

---
+++ lab8/checkpasswd.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAXLINE 256
+#define MAX_PASSWORD 10
+
+#define SUCCESS "Password verified\n"
+#define INVALID "Invalid password\n"
+#define NO_USER "No such user\n"
+
+int main(void) {
+  char user_id[MAXLINE];
+  char password[MAXLINE];
+
+  if(fgets(user_id, MAXLINE, stdin) == NULL) {
+      perror("fgets");
+      exit(1);
+  }
+  if(fgets(password, MAXLINE, stdin) == NULL) {
+      perror("fgets");
+      exit(1);
+  }
+  
+
+
+  return 0;
+}
